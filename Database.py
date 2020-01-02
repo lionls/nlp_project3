81ed6663622ba46273fe9e8cecc04250add7f20b
lionls <lio.schock@gmail.com>
2020-01-02 22:24:07 +0100

    database

--- Database.py
+++ Database.py
@@ -13,20 +13,20 @@ class Database(object):
     def knn(self, query, k):
         cos_sim = 1 - np.dot(self.documents, query.embedding.data.numpy()) / (np.linalg.norm(query.embedding.data.numpy()) * np.linalg.norm(self.documents))
         k_best_indices = np.argpartition(cos_sim,k)[:k]
-    
-        print(k_best_indices)
-    #return topk, [cos[int(i)] for i in topk]
+
+        return [(cos_sim[int(i)],i) for i in k_best_indices]
     
     def load_documents_into_embedding(self):
         print("Embedding ",len(self.documents), " Documents")
         self.documents = [Sentence(elem) for elem in self.documents]
 
-        self.documents = self.documents[0:10] ##delete later just for faster testing
+        #self.documents = self.documents[0:10] ##delete later just for faster testing
         
         self.embedding.embed(self.documents)
     
         self.documents = [elem.embedding.data.numpy() for elem in self.documents]
     
+        print(self.documents)
     
     def run_query(self, query, k=None):
         """Run a query on the given documents based on word embeddings
@@ -52,26 +52,41 @@ class Database(object):
         
         sentence = Sentence(query)
         
-        self.elmo.embed(sentence)
-        print(sentence.embedding.data.numpy())
-        #print(self.knn(sentence,k=1))
+        self.embedding.embed(sentence)
         
-
         # A returned list should look like this for k=5. Btw. the numbers are made up!
-        return [
-            (0.89316645860672, 1567),
-            (0.6174346804618835, 125),
-            (0.5975501537321234, 1181),
-            (0.5779426293373108, 3979),
-            (0.5110726475715637, 7155),
-        ]
+        
+        #[
+        #            (0.89316645860672, 1567),
+        #            (0.6174346804618835, 125),
+        #            (0.5975501537321234, 1181),
+        #            (0.5779426293373108, 3979),
+        #            (0.5110726475715637, 7155),
+        #        ]
 
+
+        return self.knn(sentence, k=k)
+            
+            
+            
+    def run_query_txt(self,text):
+        self.queries = np.loadtxt(text, delimiter='\n',dtype = str)
+        
+        results = []
+
+        for query in self.queries:
+            out = self.run_query(query)
+            results.append(out)
+
+        print(results)
 
 def main():
     data = Database(docs = "documents.txt")
-    #data.load_documents_into_embedding()
+    data.load_documents_into_embedding()
     
-    data.run_query("hallo")
+    #print(data.run_query("hallo"))
+    
+    data.run_query_txt("queries.txt")
     pass
 
 if __name__ == "__main__":
